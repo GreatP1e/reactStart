87e63ceab2da78cc561b313b852c2b4b9e4446d7
Mikhail <mig696@gmail.com>
2024-02-11 21:19:23 +0700

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { createElement } from 'react'
+import Picture from './components/picture/Picture.jsx'
+import './App.css'
+
+const App = () => {
+      return (
+        /* ... */('main', {className: /* ... */}, [/* ... */('h1', null, 'Well done!'), </* ... *//>])
+      )
+}
+
+export default App
---
+++ src/components/picture/Picture.jsx
@@ -0,0 +1,9 @@
+import img from '../../assets/img/Done.jpg'
+
+function Picture () {
+      /* ... */ (
+            <img src={img} alt="Done" />
+      )
+}
+
+export default Picture;
